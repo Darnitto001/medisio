5efe64b65bc783d08064953d7c5099376b5a97bb
Darnitto001 <danielwamela@gmail.com>
2025-03-12 11:10:33 +0300

    next commit

--- Hospitalapp/urls.py
+++ Hospitalapp/urls.py
@@ -5,7 +5,7 @@ from Hospitalapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='Home'),
+    path('Home/', views.index, name='Home'),
     path('service/', views.service, name='service'),
     path('starter/', views.starter, name='starter'),
     path('about/', views.about, name='about'),
@@ -16,5 +16,7 @@ urlpatterns = [
     path('show/', views.show, name='show'),
     path('delete/<int:id>', views.delete),
     path('edit/<int:id>', views.edit, name='edit'),
+    path('',views.register,name='register' ),
+    path('login/',views.login_view,name='login' ),
 
 ]
